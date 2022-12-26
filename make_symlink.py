81e6452888439533a163cedf02a26131d2d8084f
Yingchi Long <i@lyc.dev>
2022-12-27 00:23:50 +0800

    [script] use os.walk instead of self-invented wheel

--- make_symlink.py
+++ make_symlink.py
@@ -2,39 +2,21 @@
 
 import logging
 import os
-import re
 from argparse import ArgumentParser
 from typing import Optional
 
-from genericpath import isdir, isfile
-
-registry = {
-    # TMUX could choose .tmux.conf and ~/.config/tmux/tmux.conf
-    # ~/.config/tmux/tmux.conf is hardcoded, not XDG_CONFIG_HOME
-    # See https://wiki.archlinux.org/title/tmux
-    '.tmux.conf': "{}/.config/tmux/tmux.conf".format(os.environ['HOME'])
-}
-
-
-def find_target(s) -> Optional[str]:
-    if s in registry:
-        return registry[s]
-    if re.match(r'(.*)bash', s) is not None \
-            or s in {".zshrc", ".zprofile"}:
-        # These files just need a $HOME prefix
-        return "{}/{}".format(os.environ['HOME'], s)
+def findtarget(root: str, name: str) -> Optional[str]:
+    if name.endswith('.tmux.conf'):
+        # Hardcoded ~/.config/tmux, $XDG_CONFIG_HOME does not work.
+        # https://wiki.archlinux.org/title/tmux
+        return "{}/.config/tmux/tmux.conf".format(os.getenv("HOME"))
+    if name in {".zshrc", ".zprofile", ".p10k.zsh"}:
+        # ${ZDOTDIR:-"$HOME"}
+        return "{}/{}".format(os.getenv("ZDOTDIR", os.getenv("HOME")), name)
     return None
 
 
-ignore_pattern = [r'(.*)\.git']
-
-
-def make_link(name: str, path: str, force):
-    _target = find_target(name)
-    if _target is None:
-        logging.info(f"Ignoring file: {path}, name: {name}")
-        return
-    target: str = _target
+def makelink(target: str, path: str, force):
     logging.info(f"Linking {path} => {target}")
     path = os.path.abspath(path)
 
@@ -48,53 +30,29 @@ def make_link(name: str, path: str, force):
         else:
             logging.warning(f"Path {target} already exists, skipped.")
             logging.warning("use -f to force generate")
+            return
 
     os.symlink(path, target)
-
-
-
-def check_ignore(path : str):
-    for p in ignore_pattern:
-        if re.match(p, path) is not None:
-            logging.info(
-                f"Path {path} was ignored by pattern {p}.\n")
-            return False
-
-    return True
-
-
-def walk(path: str, max_depth: int, force: bool):
-    # A set of paths that we should ignore
-    skip = {'.git'}
-
-    # recursive DFS walk
-    def vis_walk(path, depth):
-        if depth > max_depth:
-            logging.error(
-                f"Walk depth limit exceeded! current path: {path}.")
-            return
-        skip.add(path)
-        for item in os.listdir(path):
-            full_path = os.path.join(path, item)
-            if isfile(full_path):
-                make_link(item, full_path, force)
-            elif isdir(full_path) \
-                    and full_path not in skip \
-                    and check_ignore(full_path):
-                vis_walk(full_path, depth + 1)
-
-    vis_walk(path, 1)
-
 
 def main():
     logging.basicConfig(level=logging.INFO)
     parser = ArgumentParser()
     parser.add_argument('-f', '--force', action='store_true', default=False)
-    parser.add_argument('-w', '--walk', default=os.path.dirname(__file__))
+    parser.add_argument('-w', '--walk', default='.')
     parser.add_argument('-d', '--depth', default=20)
     args = parser.parse_args()
-    walk(args.walk, args.depth, args.force)
 
+    def handlepath(root: str, name: str):
+        fullpath = os.path.join(root, name)
+        target = findtarget(root, name)
+        if target is not None:
+            makelink(target, fullpath, args.force)
+
+    for root, dirs, files in os.walk(args.walk, topdown=True):
+        for name in files:
+            handlepath(root, name)
+        for name in dirs:
+            handlepath(root, name)
 
 if __name__ == '__main__':
     main()
